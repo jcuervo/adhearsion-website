716c22f6b38410b071c32962420ce30f33def4c8
root <root@domU-12-31-39-03-48-96.compute-1.internal>
2011-02-23 08:37:31 -0500

    Remove user signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users, :member => { :suspend => :put, :unsuspend => :put, :purge => :delete }
 
   map.resource :session
-
-  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
 
   map.connect '/api/username_from_md5/:identifier_hash', :controller => "api", :action => "username_from_md5"
 
@@ -15,8 +10,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect '/api/sandbox_test', :controller => "api", :action => "sandbox_test"
 
-  map.account "/account", :controller => "users", :action => "account"
-  
   map.page ":action", :controller => "page"
   
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
