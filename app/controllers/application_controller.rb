5497d46e0c91db9d110ddd6a649acf6ee1ade2fe
Ben Langfeld <ben@langfeld.me>
2011-03-28 00:46:49 +0100

    Add back jQuery and clean up pulling blog posts from Yahoo pipes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
@@ -7,22 +9,6 @@ class ApplicationController < ActionController::Base
 
   def check_uri
     redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri if /^www/.match(request.host)
-  end
-
-  def load_blog_posts_from_aggregator
-    feed_url = "http://pipes.yahoo.com/pipes/pipe.run?_id=7d727342ec97cb855c218e5daba3843c&_render=rss"
-    feed_content = open(feed_url).read
-
-    parsed_feed = Hash.from_xml feed_content
-    feed_items = parsed_feed["rss"]["channel"]["item"]
-
-    normalized = feed_items.to_a.map do |feed_item|
-      title    = feed_item["title"]
-      pub_date = DateTime.parse(feed_item["pubDate"]) rescue Time.now
-      url      = feed_item["link"]
-      p feed_item["guid"]
-      {:title => title, :date => pub_date, :url => url}
-    end
   end
 
 end
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,7 +2,10 @@ class PageController < ApplicationController
 
   def index
     @title = "Open-Source Telephony Development Framework"
-    @blog_posts = load_blog_posts_from_aggregator
+    @blog_posts = Hash.from_xml(open("http://pipes.yahoo.com/pipes/pipe.run?_id=7d727342ec97cb855c218e5daba3843c&_render=rss").read)["rss"]["channel"]["item"].to_a.map do |feed_item|
+      pub_date = DateTime.parse(feed_item["pubDate"]) rescue Time.now
+      { :title => feed_item["title"], :date => pub_date, :url => feed_item["link"] }
+    end
   end
 
   def getting_started
